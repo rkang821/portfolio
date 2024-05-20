535a6dc82939179e566eb1db98c87df7058cf8cc
Rachel Kang <kpxrachel@gmail.com>
2024-05-19 21:47:26 -0500

    rename blog metadata for build

--- src/pages/blogs/index.ts
+++ src/pages/blogs/index.ts
@@ -1,6 +1,6 @@
 import { type BlogPostProps } from '@/models/blog'
-import { blog as cloudCaseStudyMetadata } from './cloud-case-study.mdx'
-import { blog as dfirCaseStudyMetadata } from './dfir-case-study.mdx'
+import { cloudCaseStudyMetadata } from './cloud-case-study.mdx'
+import { dfirCaseStudyMetadata } from './dfir-case-study.mdx'
 
 const postMetadata: BlogPostProps[] = [
   cloudCaseStudyMetadata,
